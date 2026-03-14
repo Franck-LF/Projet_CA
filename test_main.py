aa0d76965ff7aea3163450de4e136223463d2d5c
Franck-LF <f7lefur@gmail.com>
2026-03-14 12:32:16 +0100

    Uncomment all tests for Github Action

--- test_main.py
+++ test_main.py
@@ -47,45 +47,45 @@ def test_model_file():
         assert False, f"Le fichier {model_path} est introuvable."
 
 # S'assurer que l'on peut charger le modèle
-# def test_load_model():
-#     print("\n****** Test Load Model")
-#     try:
-#         model_path = PATH + "reglog_ia_texts.pkl"
-#         _ = Load_Pickle(model_path)
-#         assert True
-#     except:
-#         assert False, f"Impossible de charger le modèle depuis {model_path}."
+def test_load_model():
+    print("\n****** Test Load Model")
+    try:
+        model_path = PATH + "reglog_ia_texts.pkl"
+        _ = Load_Pickle(model_path)
+        assert True
+    except:
+        assert False, f"Impossible de charger le modèle depuis {model_path}."
 
 # S'assurer que le fichier de vectorizer existe bien
-# def test_vectorizer_file():
-#     print("\n****** Test Vectorizer File")
-#     try:
-#         vectorizer_path = PATH + "vectorizer_ia_texts.pkl"
-#         assert Path(vectorizer_path).exists()
-#     except:
-#         assert False, f"Le fichier {vectorizer_path} est introuvable."
+def test_vectorizer_file():
+    print("\n****** Test Vectorizer File")
+    try:
+        vectorizer_path = PATH + "vectorizer_ia_texts.pkl"
+        assert Path(vectorizer_path).exists()
+    except:
+        assert False, f"Le fichier {vectorizer_path} est introuvable."
 
-# # S'assurer que l'on peut charger le vectorizer
-# def test_load_vectorizer():
-#     print("\n****** Test Load Vectorizer")
-#     try:
-#         vectorizer_path = PATH + "vectorizer_ia_texts.pkl"
-#         _ = Load_Pickle(vectorizer_path)
-#         assert True
-#     except:
-#         assert False, f"Impossible de charger le vectorizer depuis {vectorizer_path}."
+# S'assurer que l'on peut charger le vectorizer
+def test_load_vectorizer():
+    print("\n****** Test Load Vectorizer")
+    try:
+        vectorizer_path = PATH + "vectorizer_ia_texts.pkl"
+        _ = Load_Pickle(vectorizer_path)
+        assert True
+    except:
+        assert False, f"Impossible de charger le vectorizer depuis {vectorizer_path}."
 
 # # S'assurer que la forme de sortie du vectorizer est cohérente avec 
 # # la forme d'entrée du classifieur (Logistic regression)
-# def test_vectorizer_output_dimension():
-#     vectorizer_path = PATH + "vectorizer_ia_texts.pkl"
-#     vectorizer = Load_Pickle(vectorizer_path)
-#     model_path = PATH + "reglog_ia_texts.pkl"
-#     model = Load_Pickle(model_path)
-#     X_transformed = vectorizer.transform(["Simple texte pour faire mon test"])
-#     print("TF-IDF output:", X_transformed.shape)
-#     print("Classifier input:", model.coef_.shape[1])
-#     assert X_transformed.shape[1] == model.coef_.shape[1], f"ATTENTION !!!\n {X_transformed.shape[1]} != {model.coef_.shape[1]}"
+def test_vectorizer_output_dimension():
+    vectorizer_path = PATH + "vectorizer_ia_texts.pkl"
+    vectorizer = Load_Pickle(vectorizer_path)
+    model_path = PATH + "reglog_ia_texts.pkl"
+    model = Load_Pickle(model_path)
+    X_transformed = vectorizer.transform(["Simple texte pour faire mon test"])
+    print("TF-IDF output:", X_transformed.shape)
+    print("Classifier input:", model.coef_.shape[1])
+    assert X_transformed.shape[1] == model.coef_.shape[1], f"ATTENTION !!!\n {X_transformed.shape[1]} != {model.coef_.shape[1]}"
 
 
 
