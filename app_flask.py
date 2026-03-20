5084e6dd61fa6cc05f03298517fee1d9b8e4ec07
Franck-LF <f7lefur@gmail.com>
2026-03-20 13:49:03 +0100

    1 file deleted, update app_flask launcher

--- app_flask.py
+++ app_flask.py
@@ -110,9 +110,9 @@ dashboard.config.init_from(file=BASE_DIR + '\\config.cfg')
 dashboard.bind(app)
 
 
-if __name__ == "__main__":
-    app.run(debug=True, port=5000)
-
 # if __name__ == "__main__":
-#     port = int(os.environ.get("PORT", 5000))  # prend le port fourni par Render
-#     app.run(host="0.0.0.0", port=port)
+#     app.run(debug=True, port=5000)
+
+if __name__ == "__main__":
+    port = int(os.environ.get("PORT", 5000))  # prend le port fourni par Render
+    app.run(host="0.0.0.0", port=port)
